05eac271892fbe2453e6815e2df93948b0973ad9
voltagit <pitawat76@gmail.com>
2022-07-17 17:03:32 +0700

    node_modules2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 import HomePage from './pages/home';
 import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
@@ -6,10 +7,12 @@ import Navigation from './components/navigation';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
-      <HomePage />
-    </div>
+      <Route path='/:type?' >
+        <HomePage /> 
+      </Route>
+    </Router>
   );
 }
 
